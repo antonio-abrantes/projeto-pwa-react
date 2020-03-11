922a4b8bec42930d3b47ba27e2b9619325b48272
tonhus <antonio_abrantes@hotmail.com.br>
2020-03-11 15:34:29 -0300

    webpack init config ok

---
+++ front-end/src/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+import './img/favicon.ico';
+import './css/index.css';
+import './css/pure-min.css';
+
+ReactDOM.render(
+    <h2>Bem-vindo ao React!</h2>,
+    document.querySelector("#main")
+)
