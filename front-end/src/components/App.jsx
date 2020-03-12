14d94e8c1f0458e2e092edd04db90629fcc75d46
tonhus <antonio_abrantes@hotmail.com.br>
2020-03-12 16:40:54 -0300

    NovoUsuario component ok

--- front-end/src/components/App.jsx
+++ front-end/src/components/App.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import Header from './Header';
+import NovoUsuario from './NovoUsuario';
 
 class App extends React.Component{
     render(){
         return(
             <div>
-                <h1>Compenente APP</h1>
-                <Header></Header>
+               <Header/>
+               <NovoUsuario />
             </div>
         );
     }
