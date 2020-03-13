8841a6f44319c7c1108479f61a4a5dbeec3cde72
tonhus <antonio_abrantes@hotmail.com.br>
2020-03-13 09:30:55 -0300

    Label component ok

--- front-end/src/components/NovoUsuario/index.jsx
+++ front-end/src/components/NovoUsuario/index.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import Label from '../Label';
 
 class NovoUsuario extends React.Component{
     render(){
         return(
             <div className="center">
+                <form className="pure-form pure-form-stacked">
+                    <Label valorInvalido='false' htmlFor="nome" texto="Quem é você?"></Label>
+                </form>
             </div>
         );
     }
